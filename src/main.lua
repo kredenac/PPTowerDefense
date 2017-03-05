cdef472c0b423c6fa692bfc11656bf5d6be0e788
kredenac <zacementirano@gmail.com>
2017-03-05 21:53:23 +0100

    React to click and set towers

--- src/main.lua
+++ src/main.lua
@@ -1,15 +1,44 @@
+--drzite tab size na 4
+--nije mnogo bitno, al slike su u /src/img
+--al ne znam da ih stavim van src a da ih ucitam
 function love.load()
-	--ucitavanje modula test.
+	--ucitavanje modula map
 	map=require("map")
-
 	map.generateEmpty(15,17)
+	--set mouse
+	love.mouse.setVisible(false)
+  	mouseImg = love.graphics.newImage("img/mouse_cursor.png")
+	--turret
+	turretImg = love.graphics.newImage("img/turret.png")
 end
 
 function love.update(dt)
-	map.mouse()
+	updateMouse()
+	updateKeyboard()
 end
 
 
 function love.draw()
 	map.draw()
+	drawMouse()
+end
+
+function drawMouse()
+	mousex,mousey=love.mouse.getPosition()
+	love.graphics.draw(mouseImg, mousex, mousey, 0, 0.5)
+end
+
+function updateMouse()
+	local x,y = map.mouse()
+	local down = love.mouse.isDown(1)
+	--kliknuto x,y polje, postavi tu turret
+	if (down and x > 0 and y > 0 and map.map[x][y].val == map.const.empty) then
+		map.map[x][y].val = map.const.turret
+	end
+end
+
+function updateKeyboard()
+	if love.keyboard.isDown("escape") then
+		love.event.quit()
+	end
 end
--- src/map.lua
+++ src/map.lua
@@ -12,67 +12,91 @@ M.color.r = 0
 M.color.g = 70
 M.color.b = 70
 
+M.const={}
+M.const.empty=0
+M.const.wall=1
+M.const.turret=2
+
+turret={}
+turret.color={}
+turret.color.r=M.color.r
+turret.color.g=M.color.g
+turret.color.b=M.color.b
+
 -- slicno za hover
 M.colorHover = {}
 M.colorHover.r = 0
 M.colorHover.g = 170
 M.colorHover.b = 170
 
--- Generise praznu mapu, logicno
+-- Generise praznu mapu
 -- postavlja chunkW i chunkH, sto je visina i sirina svakog pravougaonika
 -- TODO screen width i height verovatno treba da se stave na neko globalnije mesto
 
 function M.generateEmpty(width,height)
-  for i=1,width do
-    M.map[i] = {}
-    for j=1,height do
-      M.map[i][j] = {}
-      M.map[i][j].val = 0
-      M.map[i][j].hover = 0
+    for i=1,width do
+        M.map[i] = {}
+        for j=1,height do
+            M.map[i][j] = {}
+            M.map[i][j].val = M.const.empty
+            M.map[i][j].hover = false
+        end
     end
-  end
 
-  M.map.width = width
-  M.map.height = height
+    M.map.width = width
+    M.map.height = height
 
-  screen = {}
-  screen.width = love.graphics.getWidth()
-  screen.height = love.graphics.getHeight()
+    screen = {}
+    screen.width = love.graphics.getWidth()
+    screen.height = love.graphics.getHeight()
 
-  chunkW = screen.width / M.map.width
-  chunkH = screen.height / M.map.height
+    chunkW = screen.width / M.map.width
+    chunkH = screen.height / M.map.height
 
 end
 
 -- Promeni boju chunka na hover
--- TODO optimizovati verovatno, akcija na klik, mozda razdvojiti u fje
+-- moze matematicki da se izracuna u kom je polju, umesto for petlje
+-- mada nije mnogo bitno jer je mali grid
+-- TODO akcija na klik, mozda razdvojiti u fje
 function M.mouse()
-	mouseX, mouseY = love.mouse.getPosition()
-  for i=1 , M.map.width do
-    for j=1 , M.map.height do
-      isx = mouseX > (i-1)*chunkW and mouseX < i*chunkW
-    	isy = mouseY > (j-1)*chunkH and mouseY < j*chunkH
-      if isx and isy then
-        M.map[i][j].hover = true
-      else
-        M.map[i][j].hover = false
-      end
+    mouseX, mouseY = love.mouse.getPosition()
+    selectedX=-1
+    selectedY=-1
+    for i=1 , M.map.width do
+        for j=1 , M.map.height do
+            isx = mouseX > (i-1)*chunkW and mouseX < i*chunkW
+            isy = mouseY > (j-1)*chunkH and mouseY < j*chunkH
+            if isx and isy then
+                M.map[i][j].hover = true
+                selectedX, selectedY = i , j
+            else
+                M.map[i][j].hover = false
+            end
+        end
     end
-  end
+    return selectedX, selectedY
 end
 
--- Iscrtava mapu, logicno
+-- Iscrtava mapu
 function M.draw()
-  for i=1 , M.map.width do
-    for j=1 , M.map.height do
-      if M.map[i][j].hover == false then
-        love.graphics.setColor(M.color.r, M.color.g, M.color.b)
-      else
-        love.graphics.setColor(M.colorHover.r, M.colorHover.g, M.colorHover.b)
-      end
-      love.graphics.rectangle("fill", (i-1)*chunkW, (j-1)*chunkH, chunkW-1, chunkH-1)
+    for i=1 , M.map.width do
+        for j=1 , M.map.height do
+            if M.map[i][j].val == M.const.turret then
+                love.graphics.setColor(255,255, 255)
+                love.graphics.draw(turretImg,  (i-1)*chunkW, (j-1)*chunkH,
+                    0, 1/(turretImg:getWidth()/(chunkW-1)),
+                    1/(turretImg:getHeight()/(chunkH-1)) )
+            elseif M.map[i][j].hover == false then
+                love.graphics.setColor(M.color.r, M.color.g, M.color.b)
+                love.graphics.rectangle("fill", (i-1)*chunkW, (j-1)*chunkH, chunkW-1, chunkH-1)
+            else
+                love.graphics.setColor(M.colorHover.r, M.colorHover.g, M.colorHover.b)
+                love.graphics.rectangle("fill", (i-1)*chunkW, (j-1)*chunkH, chunkW-1, chunkH-1)
+            end
+
+        end
     end
-  end
 end
 
 --returning the module
